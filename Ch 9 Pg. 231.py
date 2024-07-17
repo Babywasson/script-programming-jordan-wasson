937b08302173b991b574ae224c9527d71940b011
Babywasson <156252379+Babywasson@users.noreply.github.com>
2024-07-17 13:29:39 -0500

    Add files via upload

---
+++ Ch 9 Pg. 231.py
@@ -0,0 +1,12 @@
+from breezypythongui import EasyFrame
+class LayoutDemo(EasyFrame):
+    def __init__(self):
+        EasyFrame.__init__(self)
+        self.addLabel(text = "(0, 0)", row = 0, column = 0)
+        self.addLabel(text = "(0, 1)", row = 0, column = 1)
+        self.addLabel(text = "(1, 0)", row = 1, column = 0)
+        self.addLabel(text = "(1, 1)", row = 1, column = 1)
+def main():
+    LayoutDemo().mainloop()
+
+main()
