df50a4d75df4eb39c268352d871caebacd1ef12e
Babywasson <156252379+Babywasson@users.noreply.github.com>
2024-07-21 20:08:00 -0500

    Add files via upload

---
+++ Project 3 - Wasson - Ch. 9.py
@@ -0,0 +1,28 @@
+from breezypythongui import EasyFrame
+
+class temperatureCalcuator (EasyFrame):
+                
+    def __init__(self):
+        EasyFrame.__init__(self)
+        self.titleLabel = self.addLabel(text = "Temp Converter", sticky = "NSEW", row = 0, column = 0, columnspan = 4)
+        self.fahrenheitLabel = self.addLabel(text = "Fahrenheit", sticky = "W",  row = 1, column = 0, columnspan = 2)       
+        self.fahrenheitField = self.addFloatField(value = 32.0, sticky = "NSEW", row = 2, column = 0, columnspan = 2)
+        self.celsiusLabel = self.addLabel(text = "Celsius", sticky = "W", row = 1, column = 2, columnspan = 2)
+        self.celsiusField = self.addFloatField(value = 0.0, sticky = "NSEW", row = 2, column = 2, columnspan = 2)
+        self.calcuatecelsiusButton = self.addButton(text = ">>>>", row = 3, column = 0, command = self.celsiusConversion)
+        self.calcuatefahrenheitButton = self.addButton(text = "<<<<", row = 3, column = 2, command = self.fahrenheitConversion)
+
+    def celsiusConversion(self):
+        fahrenheit = self.fahrenheitField.getNumber()
+        celsius = (fahrenheit - 32) * (5/9)
+        self.celsiusField.setNumber(celsius)
+
+    def fahrenheitConversion(self):
+        celsius = self.celsiusField.getNumber()
+        fahrenheit = celsius * (9/5) + 32
+        self.fahrenheitField.setNumber(fahrenheit)
+        
+def main():
+    temperatureCalcuator().mainloop()
+
+main()
