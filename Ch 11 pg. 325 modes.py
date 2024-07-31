1f71d1bded7131c68e7c9a0c2a094aafe2577278
Babywasson <156252379+Babywasson@users.noreply.github.com>
2024-07-31 10:40:53 -0500

    Add files via upload

---
+++ Ch 11 pg. 325 modes.py
@@ -0,0 +1,27 @@
+def frequencies(lyst):
+    theDictionary = {}
+    for number in lyst:
+        count = theDictionary.get(number, 0)
+        theDictionary[number] = count + 1
+    return theDictionary
+
+def modes(lyst):
+    if len(lyst) == 0:
+        raise RuntimeError("List must be non-empty. ")
+    theDictionary = frequencies(lyst)
+    theMaximum = max(theDictionary.values())
+    result = []
+    for key in theDictionary:
+        if theDictionary[key] == theMaximum:
+            result.append(key)
+    return result
+
+def main():
+    lyst = []
+    lystData = int(input("Enter the amount of numbers in your list: "))
+    for i in range(0, lystData):
+        info = int(input("Enter a digit from the list then press enter: "))
+        lyst.append(info)
+    print(modes(lyst))
+
+main()
